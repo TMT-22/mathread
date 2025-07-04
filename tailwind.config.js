6a45e3f07fdf14c7d668c297579b5285812ac53b
TMT-22 <145514993+TMT-22@users.noreply.github.com>
2025-07-04 19:21:26 +0900

    Add Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // content 부분을 아래처럼 수정하세요.
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
